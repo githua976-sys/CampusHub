3b7b11d2d0635adef1b21c6e8a1a7181ee61f024
githua976sys <githua976@gmail.com>
2026-07-20 11:39:16 +0300

    update models.py notes

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from lecturers.models import Lecturer
+from courses.models import Course
 
 # Create your models here.
+class Note(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE
+    )
+
+    lecturer = models.ForeignKey(
+        Lecturer,
+        on_delete=models.CASCADE
+    )
+
+    file = models.FileField(upload_to='notes/')
+
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
