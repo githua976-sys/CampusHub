a923553c9b87226455035d3c8aa3afc7d6c31cd9
githua976sys <githua976@gmail.com>
2026-07-20 10:46:01 +0300

    update model.py in courses

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from departments.models import Department
 
 # Create your models here.
+
+class Course(models.Model):
+    course_code = models.CharField(max_length=20)
+    course_name = models.CharField(max_length=100)
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.course_name
