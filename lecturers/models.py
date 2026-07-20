93d537ed23bdf6237ba9a6e27597e571b500963f
githua976sys <githua976@gmail.com>
2026-07-20 11:04:51 +0300

    update models.py

--- lecturers/models.py
+++ lecturers/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from departments.models import Department
 
 # Create your models here.
+class Lecturer(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    employee_number = models.CharField(
+        max_length=30,
+        unique=True
+    )
+
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.user.username
