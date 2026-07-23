e8f6554b8147c34953c0302647f174a064199e44
githua976sys <githua976@gmail.com>
2026-07-23 13:01:06 +0300

    add views.py lecturers

--- lecturers/views.py
+++ lecturers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Lecturer
+from .serializers import LecturerSerializer
 
 # Create your views here.
+class LecturerViewSet(viewsets.ModelViewSet):
+    queryset = Lecturer.objects.all()
+    serializer_class = LecturerSerializer
