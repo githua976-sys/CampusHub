419c338d0749ca900dcdaa22c8b8158ab0359213
githua976sys <githua976@gmail.com>
2026-07-23 13:02:22 +0300

    add views.py enrollments

--- enrollments/views.py
+++ enrollments/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Enrollment
+from .serializers import EnrollmentSerializer
 
 # Create your views here.
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
