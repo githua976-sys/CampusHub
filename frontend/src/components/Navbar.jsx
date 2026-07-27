884e037ece3d912d4f49738897a5957affe0ee02
githua976sys <githua976@gmail.com>
2026-07-27 12:23:05 +0300

    update navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <div
+            style={{
+                background: "#1E3A8A",
+                color: "white",
+                padding: "15px"
+            }}
+        >
+            <h2>CampusHub</h2>
+        </div>
+    );
+}
