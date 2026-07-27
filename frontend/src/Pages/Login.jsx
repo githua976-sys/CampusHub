c34cf425ab2d2726345744a5fbfc39e6ae799621
githua976sys <githua976@gmail.com>
2026-07-27 12:28:50 +0300

    update login.jsx

---
+++ frontend/src/Pages/Login.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+        <div>
+            <h1>CampusHub Login</h1>
+            <p>Welcome! Please log in to continue.</p>
+        </div>
+    );
+}
