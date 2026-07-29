ea5bda261d4448995d9bee534284c9d6b0214c3a
githua976sys <githua976@gmail.com>
2026-07-29 10:42:00 +0300

    update serializers.py in accounts

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from .models import Profile
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class ProfileSerializer(serializers.ModelSerializer):
-
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Profile
-        fields = "__all__"
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "role",
+        ]
