5b63ff8987071ded17a1d0a1fc55df4bf68891ef
githua976sys <githua976@gmail.com>
2026-07-29 10:46:07 +0300

    update urls.py accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ProfileViewSet
-
-router = DefaultRouter()
-router.register(r'profiles', ProfileViewSet)
+from django.urls import path
+from .views import MeView
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("me/", MeView.as_view(), name="me"),
 ]
