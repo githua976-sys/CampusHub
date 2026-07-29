773ec48e330ec33c89f33dad4c1cc15d76ffd56f
githua976sys <githua976@gmail.com>
2026-07-29 14:58:21 +0300

    update protectedroute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,19 +1,21 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-export default function ProtectedRoute() {
+export default function ProtectedRoute({ allowedRoles }) {
     const { user, loading } = useAuth();
 
-    // Wait until AuthContext finishes loading
     if (loading) {
         return <h2>Loading...</h2>;
     }
 
-    // User is not logged in
     if (!user) {
         return <Navigate to="/login" replace />;
     }
 
-    // User is authenticated
+    // Check user's role
+    if (!allowedRoles.includes(user.role)) {
+        return <Navigate to="/unauthorized" replace />;
+    }
+
     return <Outlet />;
 }
