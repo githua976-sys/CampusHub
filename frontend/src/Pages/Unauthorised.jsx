168608139ad22b621339badb064b6fdfc1b0ecd6
githua976sys <githua976@gmail.com>
2026-07-29 15:00:01 +0300

    add unauthorised.jsx

---
+++ frontend/src/Pages/Unauthorised.jsx
@@ -0,0 +1,9 @@
+export default function Unauthorized() {
+    return (
+        <div style={{ textAlign: "center", marginTop: "100px" }}>
+            <h1>403</h1>
+            <h2>Access Denied</h2>
+            <p>You do not have permission to access this page.</p>
+        </div>
+    );
+}
