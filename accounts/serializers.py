a8a472c148c2d78e73aa8e2f7d5bbd81241623f7
githua976sys <githua976@gmail.com>
2026-07-30 15:41:20 +0300

    update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+# Used for /api/users/
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+        ]
 
+
+# Used for /api/me/
 class UserSerializer(serializers.ModelSerializer):
     role = serializers.CharField(source="profile.role", read_only=True)
     phone = serializers.CharField(source="profile.phone", read_only=True)
