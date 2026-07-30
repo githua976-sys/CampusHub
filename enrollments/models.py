6509a311675285394085f73576b692b98dd27a0d
githua976sys <githua976@gmail.com>
2026-07-30 18:26:08 +0300

    update enrollments models.py

--- enrollments/models.py
+++ enrollments/models.py
@@ -2,16 +2,18 @@ from django.db import models
 from students.models import Student
 from courses.models import Course
 
-# Create your models here.
+
 class Enrollment(models.Model):
     student = models.ForeignKey(
         Student,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="enrollments"
     )
 
     course = models.ForeignKey(
         Course,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="enrollments"
     )
 
     enrolled_on = models.DateField(auto_now_add=True)
