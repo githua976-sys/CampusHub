87bd61fc853490813528f1e6928e6bf5d796583f
githua976sys <githua976@gmail.com>
2026-07-30 12:21:47 +0300

    update layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,16 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
 
 export default function Layout() {
     return (
         <>
             <Navbar />
-            <Sidebar />
 
-            <div style={{ marginLeft: "220px", padding: "20px" }}>
+            <main style={{ padding: "20px" }}>
                 <Outlet />
-            </div>
+            </main>
         </>
     );
 }
