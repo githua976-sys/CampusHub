749ff95cbd2d91085495bf5f5bf42a219b51d61b
githua976sys <githua976@gmail.com>
2026-07-30 15:29:50 +0300

    update views.py accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,20 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework.serializers import ModelSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username"]
+from .serializers import UserSerializer
+
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
