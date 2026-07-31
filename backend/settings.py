23770b2fbdd208f8e2e8f024aa36023c3bc4f8de
githua976sys <githua976@gmail.com>
2026-07-31 14:05:21 +0300

    update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -167,3 +167,11 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
