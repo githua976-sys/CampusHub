1c232b4194f3632acf6338515c45704faf37b6fe
githua976sys <githua976@gmail.com>
2026-08-23 13:45:18 +0300

    update admin.py payments

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from .models import Transaction
 # Register your models here.
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "reference",
+        "student",
+        "phone_number",
+        "amount",
+        "status",
+        "mpesa_receipt_number",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "created_at",
+    )
+
+    search_fields = (
+        "reference",
+        "phone_number",
+        "mpesa_receipt_number",
+        "student__user__username",
+    )
