db0724fd6d216ee9f69c9d703f8ccc1d2148b1ee
githua976sys <githua976@gmail.com>
2026-08-24 15:25:11 +0300

    update serializers.py accounts

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -32,12 +32,19 @@ class UserSerializer(serializers.ModelSerializer):
         except Profile.DoesNotExist:
             return None
 
-
 class AdminUserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     role = serializers.ChoiceField(
         choices=Profile.ROLE_CHOICES
+    )
+
+    registration_number = serializers.CharField(
+        required=False
+    )
+
+    department = serializers.IntegerField(
+        required=False
     )
 
     class Meta:
@@ -49,11 +56,23 @@ class AdminUserCreateSerializer(serializers.ModelSerializer):
             "last_name",
             "password",
             "role",
+            "registration_number",
+            "department",
         ]
 
     def create(self, validated_data):
         role = validated_data.pop("role")
         password = validated_data.pop("password")
+
+        registration_number = validated_data.pop(
+            "registration_number",
+            None
+        )
+
+        department_id = validated_data.pop(
+            "department",
+            None
+        )
 
         user = User.objects.create_user(
             password=password,
@@ -64,5 +83,32 @@ class AdminUserCreateSerializer(serializers.ModelSerializer):
             user=user,
             role=role
         )
+
+        if role == "Student":
+
+            if not registration_number:
+                raise serializers.ValidationError({
+                    "registration_number":
+                        "Registration number is required for students."
+                })
+
+            if not department_id:
+                raise serializers.ValidationError({
+                    "department":
+                        "Department is required for students."
+                })
+
+            from departments.models import Department
+            from students.models import Student
+
+            department = Department.objects.get(
+                id=department_id
+            )
+
+            Student.objects.create(
+                user=user,
+                registration_number=registration_number,
+                department=department
+            )
 
         return user
