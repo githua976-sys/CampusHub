7974aac0173242af77295f32e0e62b881359adf2
githua976sys <githua976@gmail.com>
2026-08-24 14:48:25 +0300

    update urls.py accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, MeView
+from .views import AdminUserCreateView
 
 router = DefaultRouter()
 router.register("users", UserViewSet)
@@ -8,4 +9,9 @@ router.register("users", UserViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("me/", MeView.as_view()),
+     path(
+        "users/create/",
+        AdminUserCreateView.as_view(),
+        name="admin-user-create"
+    ),
 ]
