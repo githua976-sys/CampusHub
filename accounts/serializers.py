4025ca03146843d2e3d53f57e08e81d8317c97f7
githua976sys <githua976@gmail.com>
2026-08-24 15:10:25 +0300

    update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,10 +1,41 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+
 from .models import Profile
+
+
+class UserSerializer(serializers.ModelSerializer):
+    role = serializers.SerializerMethodField()
+    phone = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "role",
+            "phone",
+        ]
+
+    def get_role(self, obj):
+        try:
+            return obj.profile.role
+        except Profile.DoesNotExist:
+            return None
+
+    def get_phone(self, obj):
+        try:
+            return obj.profile.phone
+        except Profile.DoesNotExist:
+            return None
 
 
 class AdminUserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+
     role = serializers.ChoiceField(
         choices=Profile.ROLE_CHOICES
     )
