0bbd9bceffe3b679e2a3ed67ed973ab0ae0ec0ea
githua976sys <githua976@gmail.com>
2026-08-24 15:08:30 +0300

    update views.py accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,46 @@
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth.models import User
 
-from .serializers import AdminUserCreateSerializer
+from .models import Profile
+from .serializers import (
+    UserSerializer,
+    AdminUserCreateSerializer,
+)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        try:
+            profile = user.profile
+            role = profile.role
+            phone = profile.phone
+        except Profile.DoesNotExist:
+            role = None
+            phone = None
+
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "role": role,
+            "phone": phone,
+        })
 
 
 class AdminUserCreateView(APIView):
